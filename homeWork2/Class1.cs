606f793566d164308b46f1c16fbeafe4f869af35
Kunio-kun <turskiyan@gmail.com>
2016-05-17 23:52:22 +0300

    Shitcode

--- homeWork2/Class1.cs
+++ homeWork2/Class1.cs
@@ -12,12 +12,23 @@ namespace homeWork2
         private List<string> nameList = new List<string> { "Blue Dog", "Steel Ass", "Infernal knight", "Dirty Marta", "Leonid bone king", "Hlado Mazer" };
 
         public int Height { get; set; }
+        Random Generator = new Random();
 
+        int Height1 = Generator.Next(150, 221);
+        private static object Generator;
+
+        public Pirate(int Height)
+        {
+            this.Height1 = Height;
+        }
+        
         public int HitPoint { get; set; }
+        private int GetHitpointsByHeight(int height);
 
+        HitPoints = GetHitPointsByHeight(height)
         public int AttackDamage { get; set; }
 
-        
-         
-         }
+      
+        }
+    }
 }
