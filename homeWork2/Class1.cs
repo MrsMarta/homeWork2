d5a55aa21757c7b50673027182fed998ab3e644d
Kunio-kun <turskiyan@gmail.com>
2016-05-17 17:36:39 +0300

    Pirate class

---
+++ homeWork2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork2
+{
+    class Pirate
+    {
+        public string Name { get; set; }
+        private List<string> nameList = new List<string> { "Blue Dog", "Steel Ass", "Infernal knight", "Dirty Marta", "Leonid bone king", "Hlado Mazer" };
+
+        public int Height { get; set; }
+
+        public int HitPoint { get; set; }
+
+        public int AttackDamage { get; set; }
+
+        
+         
+         }
+}
