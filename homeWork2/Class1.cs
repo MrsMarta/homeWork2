c4aab6fff7f60f39f8305c5bab82e20dccf9aded
Kunio-kun <turskiyan@gmail.com>
2016-05-18 17:56:15 +0300

    Shicode v0.1

--- homeWork2/Class1.cs
+++ homeWork2/Class1.cs
@@ -12,23 +12,26 @@ namespace homeWork2
         private List<string> nameList = new List<string> { "Blue Dog", "Steel Ass", "Infernal knight", "Dirty Marta", "Leonid bone king", "Hlado Mazer" };
 
         public int Height { get; set; }
-        Random Generator = new Random();
 
-        int Height1 = Generator.Next(150, 221);
-        private static object Generator;
-
-        public Pirate(int Height)
-        {
-            this.Height1 = Height;
-        }
-        
         public int HitPoint { get; set; }
-        private int GetHitpointsByHeight(int height);
 
-        HitPoints = GetHitPointsByHeight(height)
         public int AttackDamage { get; set; }
 
-      
+        public Pirate()
+        {
+            Random generator = new Random();
+            Height = generator.Next(150, 221);
+            int randomNameIndex = generator.Next(0, nameList.Count);
+            HitPoint = Height + 50;
+            AttackDamage = (Height + HitPoint) / 10;
+            Name = randomNameIndex
+
         }
+        
+        
+        
+
+      
+        
     }
 }
